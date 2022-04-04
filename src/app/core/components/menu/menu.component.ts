0b2d3a2bf218a015df7f88de9366b66e30309683
Denis Valcke <denis.valcke@studiohyperdrive.be>
2022-04-04 15:03:26 +0200

    feat: change launch-table and menu change-detection to OnPush

--- src/app/core/components/menu/menu.component.ts
+++ src/app/core/components/menu/menu.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 import { IMenuItem } from './menu.types';
 
 @Component({
   templateUrl: './menu.component.html',
   selector: 'app-menu',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MenuComponent {
   @Input() public menuItems: IMenuItem[] = [];
